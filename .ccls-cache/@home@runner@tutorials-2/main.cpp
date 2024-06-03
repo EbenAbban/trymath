16766bf3a1672e0eefc3ad480a03f49e4b2b6459
EbenAbban <abbaneben07@gmail.com>
2024-06-03 14:15:15 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@tutorials-2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iostream>
+using namespace std;
+int main() {
+  double area,radius;
+  cout<<"Enter the radius of the circle:";
+  cin>>radius;
+  area=3.14*radius*radius;
+  
+  
+  
+}
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+  cout<<"Hey There,welcome to "
+  double area,radius;
+  cout<<"Enter the radius:";
+  cin>>radius;
+  const double pi=3.14;
+  area=pi*pow(radius,2);
+  cout<<"the area of the circle is: "<<area ;
+  return 0;
+  
+  
+  
+}
